f097faf9d179742972cad34002c2b8a3ddcf95b1
paul58914080 <paul58914080@gmail.com>
2019-05-28 02:10:54 +0530

    build(todo-app): added routes, home to layout module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LayoutComponent } from './layout.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LayoutComponent} from './layout.component';
+import {AppRoutingModule} from './routes/routes.module';
+import {HomeModule} from '../home/home.module';
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [
+    LayoutComponent
+  ],
+  exports: [
+    LayoutComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule,
+    HomeModule
   ]
 })
 export class LayoutModule { }
