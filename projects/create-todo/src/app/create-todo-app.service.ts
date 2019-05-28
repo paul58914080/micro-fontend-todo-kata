f2b241c4fdcf68d4fc674f80030470a814de0051
paul58914080 <paul58914080@gmail.com>
2019-05-28 15:15:39 +0530

    feat(create-todo): added api for creating todo

--- projects/create-todo/src/app/create-todo-app.service.ts
+++ projects/create-todo/src/app/create-todo-app.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Todo} from './todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreateTodoAppService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  create(todo: Todo): Observable<void> {
+    return this.httpClient.post<void>('/todo', todo);
+  }
 }
