8454bf9a7e8416a7299a955a050f46ed13064ee9
Paul <paul58914080@gmail.com>
2019-05-30 17:44:14 +0530

    fix(create-todo): fixed the issue of aot

--- projects/create-todo/src/app/create-todo-app.module.ts
+++ projects/create-todo/src/app/create-todo-app.module.ts
@@ -6,22 +6,19 @@ import {CreateTodoAppService} from './create-todo-app.service';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserModule} from '@angular/platform-browser';
 
-const requiredModules = [CommonModule, FormsModule, HttpClientModule];
 
-const createTodoModuleConfig = isBrowserRequired => {
-  return {
-    declarations: [CreateTodoAppComponent],
-    providers: [CreateTodoAppService],
-    bootstrap: [CreateTodoAppComponent],
-    exports: [CreateTodoAppComponent],
-    imports: isBrowserRequired ? [...requiredModules, BrowserModule] : requiredModules
-  };
-};
-
-@NgModule(createTodoModuleConfig(false))
+@NgModule({
+  declarations: [ CreateTodoAppComponent ],
+  providers: [ CreateTodoAppService ],
+  exports: [ CreateTodoAppComponent ],
+  imports: [ CommonModule, FormsModule, HttpClientModule ]
+})
 export class CreateTodoAppModule {
 }
 
-@NgModule(createTodoModuleConfig(true))
+@NgModule({
+  imports: [ CreateTodoAppModule, BrowserModule ],
+  bootstrap: [ CreateTodoAppComponent ]
+})
 export class CreateTodoAppModuleWithBrowser {
 }
