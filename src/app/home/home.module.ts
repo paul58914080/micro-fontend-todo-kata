8345a48cac2dbd9305f334bcd0231b674785d874
paul58914080 <paul58914080@gmail.com>
2019-05-30 14:11:14 +0530

    feat(create-todo): integrated with main app

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home.component';
+import {CreateTodoAppModule} from '../../../projects/create-todo/src/app/create-todo-app.module';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CreateTodoAppModule
   ],
   bootstrap: [
     HomeComponent
