88c29a81e1aac7a097f4f055ea00484aaf1c489b
paul58914080 <paul58914080@gmail.com>
2019-06-03 15:36:14 +0530

    refactor(create-todo): renamed the file

--- projects/create-todo/src/main.ts
+++ projects/create-todo/src/main.ts
@@ -1,7 +1,7 @@
 import {enableProdMode, ViewEncapsulation} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {environment} from './environments/environment';
-import {CreateTodoAppModuleWithBrowser} from './app/create-todo-app.browser.module';
+import {CreateTodoAppModuleWithBrowser} from './app/create-todo-app-browser.module';
 
 if (environment.production) {
   enableProdMode();
