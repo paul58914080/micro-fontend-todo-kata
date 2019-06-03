714c64c35755e01a1fd75bade3c9c0706a9c127e
paul58914080 <paul58914080@gmail.com>
2019-06-03 15:07:52 +0530

    refactor(create-todo): separated the browser module

--- projects/create-todo/src/app/create-todo-app.module.ts
+++ projects/create-todo/src/app/create-todo-app.module.ts
@@ -4,21 +4,13 @@ import {CreateTodoAppComponent} from './create-todo-app.component';
 import {FormsModule} from '@angular/forms';
 import {CreateTodoAppService} from './create-todo-app.service';
 import {HttpClientModule} from '@angular/common/http';
-import {BrowserModule} from '@angular/platform-browser';
-
 
 @NgModule({
   declarations: [ CreateTodoAppComponent ],
   providers: [ CreateTodoAppService ],
   exports: [ CreateTodoAppComponent ],
-  imports: [ CommonModule, FormsModule, HttpClientModule ]
+  imports: [ CommonModule, FormsModule, HttpClientModule ],
+  entryComponents: [ CreateTodoAppComponent ]
 })
 export class CreateTodoAppModule {
-}
-
-@NgModule({
-  imports: [ CreateTodoAppModule, BrowserModule ],
-  bootstrap: [ CreateTodoAppComponent ]
-})
-export class CreateTodoAppModuleWithBrowser {
 }
--- projects/create-todo/src/main.ts
+++ projects/create-todo/src/main.ts
@@ -1,8 +1,7 @@
 import {enableProdMode, ViewEncapsulation} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-
-import {CreateTodoAppModuleWithBrowser} from './app/create-todo-app.module';
 import {environment} from './environments/environment';
+import {CreateTodoAppModuleWithBrowser} from './app/create-todo-app.browser.module';
 
 if (environment.production) {
   enableProdMode();
