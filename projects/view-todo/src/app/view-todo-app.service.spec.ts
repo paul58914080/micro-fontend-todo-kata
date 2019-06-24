e863066c3d4b9e188edba03d868cd369f4676128
paul58914080 <paul58914080@gmail.com>
2019-06-24 19:03:21 +0530

    feat(view-todo): added service

---
+++ projects/view-todo/src/app/view-todo-app.service.spec.ts
@@ -0,0 +1,77 @@
+import {TestBed} from '@angular/core/testing';
+
+import {ViewTodoAppService} from './view-todo-app.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {Todo} from './todo';
+
+describe('ViewTodoAppService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: ViewTodoAppService = TestBed.get(ViewTodoAppService);
+    expect(service).toBeTruthy();
+  });
+
+  let httpClientSpy: { get: jasmine.Spy };
+  let todoViewService: ViewTodoAppService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    })
+  );
+
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    todoViewService = TestBed.get(ViewTodoAppService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(todoViewService).toBeTruthy();
+  });
+
+  it('should be able to get all the todo\'s', () => {
+    const todos = [{completed: false, title: 'Watch Game of Thrones', id: 1}];
+    todoViewService.getAllTodo().subscribe((res: Todo[]) => {
+      expect(res).toBe(todos);
+    });
+    const req = httpMock.expectOne(`/todo`, 'call to api');
+    expect(req.request.method).toBe('GET');
+    req.flush(todos);
+    httpMock.verify();
+  });
+
+  it('should be able to get all pending actionable todo\'s', () => {
+    const todos = [{completed: false, title: 'Watch Game of Thrones', id: 1}];
+    todoViewService.getPendingTodo().subscribe((res: Todo[]) => {
+      expect(res).toBe(todos);
+    });
+    const req = httpMock.expectOne(`/todo?completed=false`, 'call to api for pending actions');
+    expect(req.request.method).toBe('GET');
+    req.flush(todos);
+    httpMock.verify();
+  });
+
+  it('should be able to get all completed todo\'s', () => {
+    const todos = [{completed: true, title: 'Watch Game of Thrones', id: 1}];
+    todoViewService.getCompletedTodo().subscribe((res: Todo[]) => {
+      expect(res).toBe(todos);
+    });
+    const req = httpMock.expectOne(`/todo?completed=true`, 'call to api for completed actions');
+    expect(req.request.method).toBe('GET');
+    req.flush(todos);
+    httpMock.verify();
+  });
+
+  it('should be able to update the todo item', () => {
+    const todo: Todo = {completed: true, title: 'Watch Game of Thrones', id: 1};
+    todoViewService.update(todo).subscribe();
+    const req = httpMock.expectOne(`/todo/1`, 'call to api for updating todo');
+    expect(req.request.method).toBe('PUT');
+    expect(req.request.body).toBe(todo);
+    req.flush({});
+    httpMock.verify();
+  });
+});
---
+++ projects/view-todo/src/app/view-todo-app.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Todo} from './todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ViewTodoAppService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getAllTodo(): Observable<Todo[]> {
+    return this.httpClient.get<Todo[]>('/todo');
+  }
+
+  getPendingTodo(): Observable<Todo[]> {
+    return this.httpClient.get<Todo[]>('/todo?completed=false');
+  }
+
+  getCompletedTodo(): Observable<Todo[]> {
+    return this.httpClient.get<Todo[]>('/todo?completed=true');
+  }
+
+  update(todo: Todo): Observable<void> {
+    return this.httpClient.put<void>(`/todo/${todo.id}`, todo);
+  }
+}
