085bfbd1e710ca98eb704a0c533f95870877bdac
paul58914080 <paul58914080@gmail.com>
2019-06-25 19:29:03 +0530

    fix(view-todo): added support for web-components

--- projects/view-todo/src/app/view-todo-app.module.ts
+++ projects/view-todo/src/app/view-todo-app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {Injector, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ViewTodoAppComponent} from './view-todo-app.component';
 import {BrowserModule} from '@angular/platform-browser';
@@ -6,20 +6,23 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule} from '@angular/forms';
 import {ViewTodoAppService} from './view-todo-app.service';
+import {createCustomElement} from '@angular/elements';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [ViewTodoAppComponent],
-  imports: [
-    CommonModule,
-    BrowserModule,
-    HttpClientModule,
-    NgbDropdownModule,
-    FormsModule
-  ],
+  imports: [CommonModule, BrowserModule, HttpClientModule, NgbDropdownModule, FormsModule],
   providers: [ViewTodoAppService],
   entryComponents: [ViewTodoAppComponent],
-  bootstrap: [ViewTodoAppComponent],
+  bootstrap: environment.loadBootstrap ? [ViewTodoAppComponent] : [],
   exports: [ViewTodoAppComponent]
 })
 export class ViewTodoAppModule {
+  constructor(private injector: Injector) {
+  }
+
+  ngDoBootstrap() {
+    const createTodoElement = createCustomElement(ViewTodoAppComponent, {injector: this.injector});
+    customElements.define('view-todo-element', createTodoElement);
+  }
 }
