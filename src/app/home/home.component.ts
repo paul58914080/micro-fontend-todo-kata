26c5bd4699ab6cc7d1d1e8a19cfa41e38db76678
Paul <paul58914080@gmail.com>
2019-07-18 06:35:17 +0530

    refactor(home): removed unwanted code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'todo-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
